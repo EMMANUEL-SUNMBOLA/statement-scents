68d14967c523493272e15215285d19e54a3879d8
Emmanuel <adedayoemmanuel729@gmail.com>
2024-05-06 11:32:01 +0100

    my genius is frightening

---
+++ main.js
@@ -0,0 +1,21 @@
+let nav = document.getElementsByTagName("nav")[0];
+let navContent = document.getElementsByClassName("nav-content")[0]
+let hamdiv = document.getElementsByClassName("hamdiv")[0]
+let hambut = document.getElementById("hambut")
+
+
+if(hamdiv.style.display !== "none"){
+
+    hamdiv.addEventListener("click", ()=>{
+        // alert("why you touch me")
+        if(hambut.classList.contains("fa-bars")){
+            hambut.classList.replace("fa-bars", "fa-xmark");
+            navContent.style.display = "flex"
+        }else{
+            hambut.classList.replace("fa-xmark", "fa-bars");
+            navContent.style.display = "none"       
+            navContent.style.transition = "all .3s ease-in-out"       
+        }
+    })
+
+}
